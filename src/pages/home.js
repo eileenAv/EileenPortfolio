086b50d5476494c1096361aa9cd521d26b9c8b9d
eileenAv <avciaylin03@gmail.com>
2025-10-03 20:34:08 -0700

    added some deatils

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
   return (
     <section className="home hero">
       <div className="hero-copy">
-        <p className="upperText">Hello, my name is</p>
-        <h1 className="hero-title">Eileen</h1>
-        <p className="hero-sub">Welcome to my portfolio! I hope you enjoy my work.</p>
+        <p className="hero-intro">Hi there! My name is</p>
+        <h1 className="hero-title">Eileen Avci</h1>
+        <p className="hero-description">I am a UX Designer and Frontend Developer. Welcome to my portfolio! </p>
+        <div className="hero-cta">
+          <button className="primary-btn" onClick={() => navigate('/projects')}>View My Work</button>
+        </div>
       </div>
     </section>
   );
